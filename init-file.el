4c09953ffc7ac2f9af70367344576e4a15475ad8
Shander Lam <shanderlam@gmail.com>
2011-05-23 11:45:40 +0800

    Add comments

--- init-file.el
+++ init-file.el
@@ -134,6 +134,7 @@
          )
        ))
 
+;; Configure etags-select
 (load-file "~/emacs/elisp/etags-select.el")
 (global-set-key "\M-?" 'etags-select-find-tag-at-point)
 (global-set-key "\M-." 'etags-select-find-tag)
