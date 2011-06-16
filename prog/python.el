f05af4b760becb4ca02647a683a6547fc7967d99
Shander Lam <shanderlam@gmail.com>
2011-06-16 17:14:01 +0800

    Add current directory to python sys.path in inferior python mode

--- prog/python.el
+++ prog/python.el
@@ -1,3 +1,16 @@
+(defun python-reinstate-current-directory ()
+  "When running Python, add the current directory ('') to the head of sys.path.
+For reasons unexplained, run-python passes arguments to the
+interpreter that explicitly remove '' from sys.path. This means
+that, for example, using `python-send-buffer' in a buffer
+visiting a module's code will fail to find other modules in the
+same directory.
+
+Adding this function to `inferior-python-mode-hook' reinstates
+the current directory in Python's search path."
+  (python-send-string "sys.path[:0] = ['']"))
+
+(add-hook 'inferior-python-mode-hook 'python-reinstate-current-directory)
 (add-hook 'python-mode-hook 'hs-minor-mode)
 
 (define-skeleton python-insert-property "Insert Python property template"
