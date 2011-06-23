23f4ac13a420e150a8da7fc435174c77692e22fa
Shander Lam <shanderlam@gmail.com>
2011-06-23 09:47:25 +0800

    Move custom functions to a separated file.

---
+++ custom_func.el
@@ -0,0 +1,37 @@
+(defun show-file-path ()
+  "Show the full path file name of current buffer in the minibuffer."
+  (interactive)
+  (message (buffer-file-name)))
+
+(defun show-file-name ()
+  "Show the file name of current buffer in the minibuffer."
+  (interactive)
+  (message (file-name-nondirectory buffer-file-name)))
+
+(defun insert-file-name ()
+  "Insert file name of current buffer to current cursor position"
+  (interactive)
+  (insert (file-name-nondirectory buffer-file-name)))
+
+(defun clear-whitespace ()
+  "Delete trailing white space, and replace tabs with spaces."
+  (interactive)
+  (delete-trailing-whitespace)
+  (untabify (point-min) (point-max)))
+
+(defun paste-from-clipboard()
+  (interactive)
+  (insert (shell-command-to-string "pbpaste")))
+
+(defun copy-to-clipboard()
+  (interactive)
+  (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc (buffer-substring (region-beginning) (region-end)))
+        (process-send-eof proc))))
+
+(defun generate-tags()
+  "Generate tags of current project"
+  (interactive)
+  (dolist (tags-table tags-table-list)
+    (start-process "tags" "*Messages*" "etags" "-R" (concat "-f" tags-table) (substring tags-table 0 -4))))
--- init-file.el
+++ init-file.el
@@ -65,8 +65,14 @@
 ;; Set indentation of sgml basic offset
 (setq sgml-basic-offset 4)
 
+;; Set F1 key for manual entry of current word
+(global-set-key [(f1)] (lambda () (interactive) (manual-entry (current-word))))
+
 ;; Load directory specified setting
 (load-file "~/emacs/conf/directories.el")
+
+;; Load custom functions
+(load-file "~/emacs/custom_func.el")
 
 ;; Load programing configurations
 (load-file "~/emacs/prog/python.el")
@@ -130,52 +136,6 @@
           '(lambda()
              (local-set-key "\r" 'etags-select-goto-tag)
              (local-set-key "o" 'etags-select-goto-tag-other-window)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Configurations for custom function
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun show-file-path ()
-  "Show the full path file name of current buffer in the minibuffer."
-  (interactive)
-  (message (buffer-file-name)))
-
-(defun show-file-name ()
-  "Show the file name of current buffer in the minibuffer."
-  (interactive)
-  (message (file-name-nondirectory buffer-file-name)))
-
-(defun insert-file-name ()
-  "Insert file name of current buffer to current cursor position"
-  (interactive)
-  (insert (file-name-nondirectory buffer-file-name)))
-
-(defun clear-whitespace ()
-  "Delete trailing white space, and replace tabs with spaces."
-  (interactive)
-  (delete-trailing-whitespace)
-  (untabify (point-min) (point-max)))
-
-;; Set F1 key for manual entry of current word
-(global-set-key [(f1)] (lambda () (interactive) (manual-entry (current-word))))
-
-(defun paste-from-clipboard()
-  (interactive)
-  (insert (shell-command-to-string "pbpaste")))
-
-(defun copy-to-clipboard()
-  (interactive)
-  (let ((process-connection-type nil))
-      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-        (process-send-string proc (buffer-substring (region-beginning) (region-end)))
-        (process-send-eof proc))))
-
-(defun generate-tags()
-  "Generate tags of current project"
-  (interactive)
-  (dolist (tags-table tags-table-list)
-    (start-process "tags" "*Messages*" "etags" "-R" (concat "-f" tags-table) (substring tags-table 0 -4))))
-
 
 ;; Let completion in a popup menu instead of new buffer
 (defcustom complete-in-region-use-popup t
