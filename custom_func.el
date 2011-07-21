daba160b51d23424201a0cce172a3b7ac1607daa
Shander Lam <shanderlam@gmail.com>
2011-07-21 11:25:03 +0800

    Modify coding style.

--- custom_func.el
+++ custom_func.el
@@ -19,18 +19,18 @@
   (delete-trailing-whitespace)
   (untabify (point-min) (point-max)))
 
-(defun paste-from-clipboard()
+(defun paste-from-clipboard ()
   (interactive)
   (insert (shell-command-to-string "pbpaste")))
 
-(defun copy-to-clipboard()
+(defun copy-to-clipboard ()
   (interactive)
   (let ((process-connection-type nil))
       (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
         (process-send-string proc (buffer-substring (region-beginning) (region-end)))
         (process-send-eof proc))))
 
-(defun generate-tags()
+(defun generate-tags ()
   "Generate tags of current project"
   (interactive)
   (dolist (tags-table tags-table-list)
