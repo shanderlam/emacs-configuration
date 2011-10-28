d5a4bafc410b35d3d219765a1673a4080d9aff8f
Shander Lam <shanderlam@gmail.com>
2011-10-28 09:46:10 +0800

    Do not automatic turn flymake mode on when entering js mode.

--- modes/javascript.el
+++ modes/javascript.el
@@ -1,4 +1,4 @@
 ;; Load js2 mode
 (autoload 'js2-mode "js2" nil t)
-(add-hook 'js-mode-hook '(lambda()
-                           (flymake-mode t)))
+;; (add-hook 'js-mode-hook '(lambda()
+;;                            (flymake-mode t)))
