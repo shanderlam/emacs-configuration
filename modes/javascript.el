c7b2d27debcbcb43856bbdf8443c27ebd16e3f4d
Shander Lam <shanderlam@gmail.com>
2011-11-06 04:37:37 +0800

    Use js3-mode for edit javascript file

--- modes/javascript.el
+++ modes/javascript.el
@@ -1,4 +1,5 @@
-;; Load js2 mode
-(autoload 'js2-mode "js2" nil t)
+;; Load js3 mode
+(autoload 'js3-mode "js3" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
 ;; (add-hook 'js-mode-hook '(lambda()
 ;;                            (flymake-mode t)))
