f6b270e6a1ea7f919252fe61bb621fb2e38c2abc
Shander Lam <shanderlam@gmail.com>
2011-11-07 10:59:31 +0800

    Set js3-indent-level with 4

--- modes/javascript.el
+++ modes/javascript.el
@@ -1,5 +1,6 @@
 ;; Load js3 mode
 (autoload 'js3-mode "js3" nil t)
+(setq-default js3-indent-level 4)
 (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
 ;; (add-hook 'js-mode-hook '(lambda()
 ;;                            (flymake-mode t)))
