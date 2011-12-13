9cfbcec531611eb4c8750e7f362e6ad9d732e681
Shander Lam <shanderlam@gmail.com>
2011-12-13 09:43:15 +0800

    Do not use color-theme-twilight for terminal.

--- init-file.el
+++ init-file.el
@@ -202,8 +202,7 @@
   (progn
       (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
       (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-      (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-      (color-theme-twilight)))
+      (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
