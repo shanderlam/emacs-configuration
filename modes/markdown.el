4d0b8ba327c0115f9b14bfc293fe819a79996fb9
Shander Lam <shanderlam@gmail.com>
2012-05-07 14:47:14 +0800

    Add markdown to auto-mode-alist.

--- modes/markdown.el
+++ modes/markdown.el
@@ -1,4 +1,5 @@
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-      (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
