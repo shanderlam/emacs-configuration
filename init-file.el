2ed44c4be90a77e6c2a6e4ca08203adec59126a8
Shander Lam <shanderlam@gmail.com>
2012-05-14 19:29:25 +0800

    Add shortcut 'Command-Control f' for ns-toggle-fullscreen.

--- init-file.el
+++ init-file.el
@@ -209,7 +209,8 @@
                                                 "latin:-*-Menlo-*-*-*-*-*-*-*-*-*-*-iso10646-1,"
                                                 "han:-*-STHeiti-medium-*-*-*-*-*-*-*-*-*-iso10646-1"))
       (add-to-list 'default-frame-alist '(font . "fontset-custom"))
-      (color-theme-twilight))
+      (color-theme-twilight)
+      (global-set-key (kbd "<C-s-268632070>") 'ns-toggle-fullscreen))
   (color-theme-shanderlam))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
