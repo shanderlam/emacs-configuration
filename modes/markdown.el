55007d155bf548b9fca46a906c1646c1ec14bcd1
Shander Lam <shanderlam@gmail.com>
2012-05-26 14:10:19 +0800

    Configure markdown-mode as submodule

--- modes/markdown.el
+++ modes/markdown.el
@@ -1,5 +1,5 @@
-(autoload 'markdown-mode "markdown-mode.el"
+(add-to-list 'load-path (concat emacs-config-dir "elisp/markdown-mode.git"))
+(autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
-
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
