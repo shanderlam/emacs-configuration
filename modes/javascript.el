35acfc2ee6e5eef0fca82b3adf2c05e4771c5553
Shander Lam <shanderlam@gmail.com>
2012-06-14 15:24:17 +0800

    Set buffer local js-indent-level for jade file.

--- modes/javascript.el
+++ modes/javascript.el
@@ -1,4 +1,4 @@
-(add-hook 'js-mode-hook '(lambda()
-                           (if (string-match "\\.jade$" buffer-file-name)
-                               (setq js-indent-level 2)
-                             (setq js-indent-level 4))))
+(add-hook 'js-mode-hook
+          '(lambda()
+             (if (string-match "\\.jade$" buffer-file-name)
+                 (set (make-local-variable 'js-indent-level) 2))))
