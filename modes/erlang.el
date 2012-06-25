2b7fe1a0219cb4f51b0d24ef7a89013071b44808
Shander Lam <shanderlam@gmail.com>
2012-06-25 11:03:42 +0800

    Use `when' instead of `if'.

--- modes/erlang.el
+++ modes/erlang.el
@@ -1,7 +1,6 @@
 ;; erlang mode
-(if (file-exists-p "/usr/local/lib/erlang/lib/tools-2.6.6.1/emacs")
-    (progn
-      (add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.6.1/emacs")
-      (setq erlang-root-dir "/usr/local/lib/erlang")
-      (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
-      (require 'erlang-start)))
+(when (file-exists-p "/usr/local/lib/erlang/lib/tools-2.6.6.1/emacs")
+  (add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.6.1/emacs")
+  (setq erlang-root-dir "/usr/local/lib/erlang")
+  (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+  (require 'erlang-start))
