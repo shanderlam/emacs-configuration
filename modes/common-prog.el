26e8bc80896fc9094f383a48c5ef13f594ef41c0
Shander Lam <shanderlam@gmail.com>
2012-08-21 14:27:45 +0800

    Remove prog-modes variable.

--- modes/common-prog.el
+++ modes/common-prog.el
@@ -1,15 +1,12 @@
-(setq prog-modes
-     '(c-mode-common-hook
-       css-mode-hook
-       emacs-lisp-mode-hook
-       erlang-mode-hook
-       html-mode-hook
-       jade-mode-hook
-       js-mode-hook
-       php-mode-hook
-       python-mode-hook))
-
-(dolist (mode prog-modes)
+(dolist (mode '(c-mode-common-hook
+                css-mode-hook
+                emacs-lisp-mode-hook
+                erlang-mode-hook
+                html-mode-hook
+                jade-mode-hook
+                js-mode-hook
+                php-mode-hook
+                python-mode-hook))
   (add-hook mode
             '(lambda ()
                (if (>= emacs-major-version 23) (linum-mode 1))
