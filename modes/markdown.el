1d728fa7e3b4bc51f52c02d2804cb24830d805cf
Shander Lam <shanderlam@gmail.com>
2012-09-19 21:09:13 +0800

    Modify markdown mode configuration.

--- modes/markdown.el
+++ modes/markdown.el
@@ -1,5 +1,6 @@
-(add-to-list 'load-path (concat emacs-config-dir "elisp/markdown-mode.git"))
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(when (require 'markdown-mode nil t)
+  (progn
+    (autoload 'markdown-mode "markdown-mode"
+      "Major mode for editing Markdown files" t)
+    (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+    (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))))
