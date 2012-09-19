813127b529e1f46e816b174cafdd3bb749f386fc
Shander Lam <shanderlam@gmail.com>
2012-09-19 19:57:14 +0800

    Fixes: jade mode configuration.

--- modes/jade.el
+++ modes/jade.el
@@ -1,6 +1,5 @@
-(add-to-list 'load-path (concat emacs-config-dir "elisp/jade-mode.git"))
-(require 'sws-mode)
-(require 'jade-mode)
-
-(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
-(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+(when (require 'sws-mode nil t)
+  (progn
+    (require 'jade-mode)
+    (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+    (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))))
