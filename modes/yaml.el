d77e2fcad84ec3ccb0a018762def793ec790a9b9
Shander Lam <shanderlam@gmail.com>
2012-09-20 15:46:23 +0800

    Modify yaml mode configuration

--- modes/yaml.el
+++ modes/yaml.el
@@ -1,3 +1,2 @@
-(add-to-list 'load-path (concat emacs-config-dir "elisp/yaml-mode.git"))
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(when (require 'yaml-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
