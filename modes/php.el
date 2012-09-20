59a019d2dbd5a367171f135f0445b3de0565a11f
Shander Lam <shanderlam@gmail.com>
2012-09-20 15:28:09 +0800

    Modify php-mode configuration

--- modes/php.el
+++ modes/php.el
@@ -1,7 +1,4 @@
-;; Enable php mode
-(add-to-list 'load-path (concat emacs-config-dir "elisp/php-mode.git"))
-(require 'php-mode)
-
-;; Automatically activate flymake mode for php mode
-(add-hook 'php-mode-hook '(lambda ()
-                            (flymake-mode 1)))
+(when (require 'php-mode nil t)
+  ;; Automatically activate flymake mode for php mode
+  (add-hook 'php-mode-hook '(lambda ()
+                              (flymake-mode 1))))
