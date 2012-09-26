9eaee07ab23149041fe1a5528de4f78205e6fabc
Shander Lam <shanderlam@gmail.com>
2012-09-26 11:53:28 +0800

    Directly require erlang-start feature.

--- modes/erlang.el
+++ modes/erlang.el
@@ -1,6 +1,2 @@
 ;; erlang mode
-(when (file-exists-p "/usr/local/lib/erlang/lib/tools-2.6.6.1/emacs")
-  (add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.6.1/emacs")
-  (setq erlang-root-dir "/usr/local/lib/erlang")
-  (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
-  (require 'erlang-start))
+(require 'erlang-start nil t)
