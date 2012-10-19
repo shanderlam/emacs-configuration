deaaf1a49234e2d16fdc7018bc919bbb00c3a46c
Shander Lam <shanderlam@gmail.com>
2012-10-19 19:06:52 +0800

    Customize ediff faces.

--- themes/original-theme.el
+++ themes/original-theme.el
@@ -15,6 +15,17 @@
  '(magit-diff-add ((t (:foreground "green"))))
  '(magit-diff-del ((t (:foreground "red"))))
 
+ ;; ediff
+ '(ediff-current-diff-A ((t (:background "yellow" :foreground "black"))))
+ '(ediff-odd-diff-A ((t (:background "brightblack" :foreground "white"))))
+ '(ediff-even-diff-A ((t (:background "brightblack" :foreground "white"))))
+ '(ediff-fine-diff-A ((t (:background "cyan" :foreground "black"))))
+
+ '(ediff-current-diff-B ((t (:background "yellow" :foreground "black"))))
+ '(ediff-odd-diff-B ((t (:background "brightblack" :foreground "white"))))
+ '(ediff-even-diff-B ((t (:background "brightblack" :foreground "white"))))
+ '(ediff-fine-diff-B ((t (:background "cyan" :foreground "black"))))
+
  ;; diff
  '(diff-added ((t (:foreground "green"))))
  '(diff-removed ((t (:foreground "red")))))
