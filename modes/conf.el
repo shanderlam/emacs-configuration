a069b9800cb981eab3059104191de9adca82c4fe
Shander Lam <shanderlam@gmail.com>
2012-11-13 07:42:04 +0800

    Make same files open with conf mode.

---
+++ modes/conf.el
@@ -0,0 +1,3 @@
+(add-to-list 'auto-mode-alist '("options\\.l2tpd$" . conf-mode))
+(add-to-list 'auto-mode-alist '("ipsec\\.secrets$" . conf-mode))
+(add-to-list 'auto-mode-alist '("ipsec\\.secrets\\.inc$" . conf-mode))
