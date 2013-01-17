4b806f1bc98d4d2554bc3080790083ac907069d8
Shander Lam <shanderlam@gmail.com>
2013-01-17 23:30:43 +0800

    Reorganize aliases.

--- aliases.el
+++ aliases.el
@@ -1,18 +1,20 @@
 (defalias 'sb 'speedbar)
-(defalias 'fn 'cus-insert-buffer-file-name)
 (defalias 'rb 'revert-buffer)
-(defalias 'ts 'cus-insert-timestamp)
-(defalias 'cls 'cus-clear-whitespace)
-(defalias 'lcp 'cus-locate-project)
-(defalias 'ctags 'cus-generate-tags)
+
+(when (fboundp 'list-packages) (defalias 'lp 'list-packages))
 
 (when (fboundp 'helm-locate) (defalias 'hlc 'helm-locate))
 (when (fboundp 'helm-imenu) (defalias 'him 'helm-imenu))
-(when (fboundp 'cus-say) (defalias 'say 'cus-say))
+
+(defalias 'cls 'cus-clear-whitespace)
+(defalias 'ctags 'cus-generate-tags)
+(defalias 'fn 'cus-insert-buffer-file-name)
+(defalias 'lcp 'cus-locate-project)
+(defalias 'ts 'cus-insert-timestamp)
+
+(when (fboundp 'cus-copy-to-clipboard)
+  (defalias 'cp 'cus-copy-to-clipboard))
 (when (fboundp 'cus-open-in-macvim) (defalias 'mvim 'cus-open-in-macvim))
 (when (fboundp 'cus-paste-from-clipboard)
   (defalias 'pst 'cus-paste-from-clipboard))
-(when (fboundp 'cus-copy-to-clipboard)
-  (defalias 'cp 'cus-copy-to-clipboard))
-
-(when (fboundp 'list-packages) (defalias 'lp 'list-packages))
+(when (fboundp 'cus-say) (defalias 'say 'cus-say))
