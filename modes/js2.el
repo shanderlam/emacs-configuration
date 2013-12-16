7c586b96e186a241f407ee8686f1dc4604098bf0
Shander Lam <shanderlam@gmail.com>
2013-12-16 16:57:09 +0800

    Disable js2-strict-inconsistent-return-warning

--- modes/js2.el
+++ modes/js2.el
@@ -1,1 +1,4 @@
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(when (require 'js2-mode nil t)
+  (progn
+	(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+	(setq js2-strict-inconsistent-return-warning nil)))
