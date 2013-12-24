0780fb48ba5cf0d4745038c18567a4e214e53559
Shander Lam <shanderlam@gmail.com>
2013-12-24 16:47:19 +0800

    Use projectile extension

--- ext-settings.el
+++ ext-settings.el
@@ -59,3 +59,5 @@
 (add-to-list 'auto-mode-alist '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
 (autoload 'csv-mode "csv-mode"
   "Major mode for editing comma-separated value files." t)
+
+(projectile-global-mode)
