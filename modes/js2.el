4890296e43e889e5a952a372dfe9cd376de44d41
Shander Lam <shanderlam@gmail.com>
2014-01-07 11:58:35 +0800

    Enable js2 bounce indent

--- modes/js2.el
+++ modes/js2.el
@@ -1,4 +1,5 @@
 (when (require 'js2-mode nil t)
   (progn
 	(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+	(setq js2-bounce-indent-p t)
 	(setq js2-strict-inconsistent-return-warning nil)))
