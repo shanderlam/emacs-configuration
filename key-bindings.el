4c053d6739a679d501784b76961d05c1ef1f14ec
Shander Lam <shanderlam@gmail.com>
2014-06-27 23:21:04 +0800

    Key binding for windows system not needed anymore

--- key-bindings.el
+++ key-bindings.el
@@ -1,9 +1,5 @@
 ;; Set F1 key for manual entry of current word
 (global-set-key [(f1)] (lambda () (interactive) (manual-entry (current-word))))
 
-(when (equal window-system 'ns)
-  ;; Set Command-control-f for toggle fullscreen
-  (global-set-key (kbd "<C-s-268632070>") 'ns-toggle-fullscreen))
-
 (when (fboundp 'cus-eval-dwim)
   (global-set-key (kbd "C-x C-e") 'cus-eval-dwim))
