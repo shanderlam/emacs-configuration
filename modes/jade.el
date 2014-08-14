4bf8235bb25242cf7c0b7e323ab641085d213704
Shander Lam <shanderlam@gmail.com>
2014-08-14 12:04:33 +0800

    Remove unused file

--- modes/jade.el
+++
@@ -1,5 +0,0 @@
-(when (require 'sws-mode nil t)
-  (progn
-    (require 'jade-mode)
-    (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
-    (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))))
